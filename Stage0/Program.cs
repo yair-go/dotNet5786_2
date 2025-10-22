95485fcd8811859a574086dead6f639e00258492
yairJCT <irigo1@gmail.com>
2025-10-22 19:30:32 +0300

    Greeting functionality

--- Stage0/Program.cs
+++ Stage0/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 
-namespace Stage0
+namespace Stage0;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-        }
+        Greeting("Yair", 30);
+        Greeting("Anna", 25);
+        Console.ReadLine();
+    }
+
+    private static void Greeting(string name, int grade)
+    {
+        Console.WriteLine($"Hello {name} age : {grade}");
     }
 }
