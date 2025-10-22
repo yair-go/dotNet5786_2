d2ad85a1f0c5967f9fc8e16cfd5570a2ae381c17
yairJCT <irigo1@gmail.com>
2025-10-22 19:00:08 +0300

    Add project files.

---
+++ Stage0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stage0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
