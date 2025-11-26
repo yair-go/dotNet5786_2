71c8c7193264d905b27c2d8e59ac6357e266cecb
yairJCT <irigo1@gmail.com>
2025-11-26 19:51:20 +0200

    Remove Class1 and add new WPF project "PL"

---
+++ PL/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using DalList;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+
+            InitializeComponent();
+
+            lv_orders.ItemsSource = InitDO.CreateSampleOrders(100); ;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) 
+            => MessageBox.Show("Hello, World!");
+
+        private void Button_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("All Right!");
+        }
+
+        private void Click_Me_Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Click_Me_Button.Content = "Click Me Now!";
+        }
+
+        private void Click_Me_Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Click_Me_Button.Content = "Click Me!";
+        }
+
+      
+    }
+}
